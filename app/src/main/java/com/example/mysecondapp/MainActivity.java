86805bc0d1fd63bbb04d5ff29c9d53605a4a9911
shohrab <you@example.com>
2019-03-06 01:00:02 +0600

    okk

--- app/src/main/java/com/example/mysecondapp/MainActivity.java
+++ app/src/main/java/com/example/mysecondapp/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.mysecondapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button aboutbtn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aboutbtn  = findViewById(R.id.btnabout);
+
+        aboutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
